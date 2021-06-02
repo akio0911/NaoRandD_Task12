24481e92e84991549a6d723f272d24d9ed5d1ebb
Nao-RandD <nao.r.and.d@gmail.com>
2021-06-03 08:38:57 +0900

    first commit.

---
+++ AppDojoTask12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDojoTask12
+//
+//  Created by Naoyuki Kan on 2021/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
