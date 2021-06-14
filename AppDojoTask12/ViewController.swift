cee9756b2ba47d68ecc8e5cc114b1440a054189d
akio0911 <akio0911@gmail.com>
2021-06-14 17:39:45 +0900

    feedback

--- AppDojoTask12/ViewController.swift
+++ AppDojoTask12/ViewController.swift
@@ -9,29 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet private weak var noTaxLabel: UITextField!
-    @IBOutlet private weak var taxRateLabel: UITextField!
+    @IBOutlet private weak var noTaxTextField: UITextField!
+    @IBOutlet private weak var taxRateTextField: UITextField!
     @IBOutlet private weak var includeTaxLabel: UILabel!
+
+    private static let taxLateKey = "TaxLate"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        taxRateLabel.text = UserDefaults.standard.string(forKey: "TaxLate") ?? ""
+
+        taxRateTextField.text = UserDefaults.standard.string(forKey: Self.taxLateKey) ?? ""
+
         // キーボードの入力を数字に設定
-        noTaxLabel.keyboardType = .numberPad
-        taxRateLabel.keyboardType = .numberPad
+        noTaxTextField.keyboardType = .numberPad
+        taxRateTextField.keyboardType = .numberPad
     }
 
     // 計算ボタンがタップされたときに走る処理
     @IBAction func tappedCalButton(_ sender: Any) {
         // 税抜金額と消費税率を取得して計算する
-        guard let noTaxAmount = Int(noTaxLabel.text!),
-              let taxRate = Int(taxRateLabel.text!) else {
+        guard let noTaxAmount = Int(noTaxTextField.text!),
+              let taxRate = Int(taxRateTextField.text!) else {
             return
         }
         let amountIncludeTax = Float(noTaxAmount) * (1.0 + Float(taxRate) / 100.0)
         // 消費税率をUserDefaultsに保存する
-        UserDefaults.standard.set(taxRate, forKey: "TaxLate")
+        UserDefaults.standard.set(taxRate, forKey: Self.taxLateKey)
 
         // 計算結果を結果ラベルに反映する
         includeTaxLabel.text = String(Int(amountIncludeTax))
